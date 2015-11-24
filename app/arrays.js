02f1a8d8d4806f47055f3cb6403c7ebd2f60be90
Khalilur Rahman <khal.rahman@gmail.com>
2015-11-25 00:05:38 +0800

    fixed array sum test

--- app/arrays.js
+++ app/arrays.js
@@ -3,7 +3,7 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.arraysAnswers = {
   indexOf : function(arr, item) {
-    
+
     for (var i = 0, len = arr.length; i < len; i++) {
       if (arr[i] === item) {
         return i;
@@ -11,5 +11,14 @@ exports.arraysAnswers = {
     }
 
     return -1;
+  },
+  sum: function(arr) {
+    var sum = 0;
+    
+    for (var i = 0, len = arr.length; i < len; i++) {
+      sum += arr[i];
+    }
+
+    return sum;
   }
 };
