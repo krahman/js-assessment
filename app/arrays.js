6201e8390bd435e72699c5437e1aa324aeb6efd1
Khalilur Rahman <khal.rahman@gmail.com>
2015-11-24 23:57:58 +0800

    solved to determine an item location in an array test

--- app/arrays.js
+++ app/arrays.js
@@ -1,60 +1,15 @@
+
 exports = (typeof window === 'undefined') ? global : window;
 
 exports.arraysAnswers = {
-
   indexOf : function(arr, item) {
+    
+    for (var i = 0, len = arr.length; i < len; i++) {
+      if (arr[i] === item) {
+        return i;
+      }
+    }
 
-  },
-
-  sum : function(arr) {
-
-  },
-
-  remove : function(arr, item) {
-
-  },
-
-  removeWithoutCopy : function(arr, item) {
-
-  },
-
-  append : function(arr, item) {
-
-  },
-
-  truncate : function(arr) {
-
-  },
-
-  prepend : function(arr, item) {
-
-  },
-
-  curtail : function(arr) {
-
-  },
-
-  concat : function(arr1, arr2) {
-
-  },
-
-  insert : function(arr, item, index) {
-
-  },
-
-  count : function(arr, item) {
-
-  },
-
-  duplicates : function(arr) {
-
-  },
-
-  square : function(arr) {
-
-  },
-
-  findAllOccurrences : function(arr, target) {
-
+    return -1;
   }
 };
